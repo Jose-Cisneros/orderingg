e23cda0fa2abb37f9438d76ec5832178c6e0511c
titiloxx <titiloxx@gmail.com>
2018-05-27 22:01:55 -0300

    Test #1 finalizado

--- test/test_e2e.py
+++ test/test_e2e.py
@@ -4,7 +4,7 @@ import time
 import threading
 
 from selenium import webdriver
-
+from selenium.webdriver.support.ui import Select
 from app import create_app, db
 from app.models import Product, Order, OrderProduct
 
@@ -36,7 +36,7 @@ class Ordering(unittest.TestCase):
 
         time.sleep(1)
 
-        self.driver = webdriver.Chrome()
+        self.driver = webdriver.Chrome('/Users/titiloxx/Desktop/Cosas papa/chromedriver 2') 
 
     def test_title(self):
         driver = self.driver
@@ -45,6 +45,22 @@ class Ordering(unittest.TestCase):
         add_product_button.click()
         modal = driver.find_element_by_id('modal')
         assert modal.is_displayed(), "El modal no esta visible"
+    
+    def test_modalEdit(self):
+        driver = self.driver 
+        driver.get(self.baseURL)
+        nombreTabla=driver.find_element_by_xpath('//*[@id="orders"]/table/tbody/tr[1]/td[2]').text
+        cantidadTabla=driver.find_element_by_xpath('//*[@id="orders"]/table/tbody/tr[1]/td[4]').text
+        precioTotalTabla=driver.find_element_by_xpath('//*[@id="orders"]/table/tbody/tr[1]/td[5]').text
+        edit_product_button = driver.find_element_by_xpath('//*[@id="orders"]/table/tbody/tr[1]/td[6]/button')
+        edit_product_button.click()
+     
+        nombreModal = Select(driver.find_element_by_id('select-prod')).first_selected_option.text
+        cantidadModal = driver.find_element_by_id('quantity').get_attribute('value')
+        precioTotalModal= driver.find_element_by_id('total-price').text.replace("Precio total: $ ","")
+        assert cantidadTabla==cantidadModal, "La cantidad no coincide"
+        assert nombreTabla==nombreModal, "El nombre no coincide"
+        assert precioTotalTabla==precioTotalModal, "El precio total no coincide"
 
     def tearDown(self):
         self.driver.get('http://localhost:5000/shutdown')
