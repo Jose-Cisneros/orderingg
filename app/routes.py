893ae6e600e1e925482d436764b5212a23cab5bf
jose-cisneros <jose.cisneros@despegar.com>
2018-06-16 15:07:10 -0300

    Mejorado el estilo de routes.py segun standard pep8

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+"""API Rest para el manejo de la aplicacion."""
 from sqlalchemy import and_
 
 from app import db
@@ -7,9 +8,12 @@ from flask import request, jsonify, render_template, abort, current_app
 from flask import Blueprint
 rest = Blueprint('rest', __name__, template_folder='templates')
 
+
 @rest.route("/")
 def hello():
+    """Renderiza la vista."""
     return render_template('orders.html')
+
 
 @rest.route("/product", methods=['GET', 'POST'])
 def products():
@@ -20,7 +24,8 @@ def products():
     if request.method == 'POST':
         # Crea un nuevo producto recibiendo un JSON con atributos name y price
         # Ejemplo: {'name': 'Tenedor', 'price': 50}
-        p = Product(name=request.get_json()['name'], price=request.get_json()['price'])
+        p = Product(name=request.get_json()['name'],
+                    price=request.get_json()['price'])
         db.session.add(p)
         db.session.commit()
         return jsonify(p.serialize)
@@ -28,6 +33,7 @@ def products():
         # Obtiene todos los productos disponibles
         p = Product.query.all()
         return jsonify([i.serialize for i in p])
+
 
 @rest.route("/order", methods=['GET'])
 def orders():
@@ -51,18 +57,19 @@ def order(pk):
 
     # Si la orden no existe, levantamos el error
     if (not order):
-        return jsonify({ 'error': 'not-found' }), 404
+        return jsonify({'error': 'not-found'}), 404
 
     return jsonify(order.serialize)
 
+
 @rest.route("/order/<pk>/product", methods=['POST'])
-def addProductToOrder(pk):
+def addproducttoorder(pk):
     # obtenemos las ordenes
     order = Order.query.get(pk)
 
     # Si la orden no existe, levantamos el error
     if (not order):
-        return jsonify({ 'error': '<order {}> not found'.format(pk) }), 404
+        return jsonify({'error': '<order {}> not found'.format(pk)}), 404
 
     product_data = request.get_json()
     product = product_data['product']
@@ -77,12 +84,12 @@ def addProductToOrder(pk):
             'error': '<product {}> exists in <order {}>. Use PUT method'
                 .format(product['id'], pk)
         }), 400
-    cantidad=product_data['quantity']
+    cantidad = product_data['quantity']
     if (int(cantidad) < 1):
         return jsonify({
                 'error': 'Quantity must be positive'
             }), 400
-	
+
     orderProduct = OrderProduct(quantity=product_data['quantity'])
     orderProduct.product = Product.query.get(product['id'])
     order.products.append(orderProduct)
@@ -92,7 +99,9 @@ def addProductToOrder(pk):
 
     return jsonify(order.serialize), 201
 
-@rest.route("/order/<pk_order>/product/<pk_product>", methods=['GET', 'PUT', 'DELETE'])
+@rest.route("/order/<pk_order>/product/<pk_product>", methods=['GET',
+                                                               'PUT',
+                                                               'DELETE'])
 def order_product_detail(pk_order, pk_product):
     """
     Obtiene un producto de una orden y modifica un producto de una orden
@@ -101,12 +110,17 @@ def order_product_detail(pk_order, pk_product):
     Si no se encuentra el producto, se responde con un 404
     """
 
-    order_product = OrderProduct.query.filter(and_(OrderProduct.order_id==pk_order, OrderProduct.product_id==pk_product)).all()
+    order_product = OrderProduct.query.filter(and_(
+                                                   OrderProduct.
+                                                   order_id == pk_order,
+                                                   OrderProduct.
+                                                   product_id == pk_product)
+                                              )    .all()
 
     if (not order_product):
         return jsonify({ 'error': 'not-found' }), 404
 
-    order_product=order_product[0]
+    order_product = order_product[0]
     order_product_json = order_product.serialize
 
     if request.method == 'PUT':
